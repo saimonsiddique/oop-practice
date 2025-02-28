184181ac25132266fe07ab89fb0dd776b861a797
saimonsiddique <saimonsiddiquee@gmail.com>
2025-02-28 22:08:15 +0600

    Refactor car rental program to use a list of vehicles and streamline rental cost calculations

--- practice_problem_session_nine/CSharp/practice_problem_twelve_onsite/CarRentalMangement/Program.cs
+++ practice_problem_session_nine/CSharp/practice_problem_twelve_onsite/CarRentalMangement/Program.cs
@@ -1,16 +1,17 @@
 ﻿Vehicle car = new Car("Toyota", "Corolla", new DateTime(2010, 2, 10), 5);
-Vehicle bike = new Bike("Yamaha", "R1", new DateTime(2010, 2, 10), 1000);
+Vehicle bike1 = new Bike("Yamaha", "R1", new DateTime(2010, 2, 10), 1000);
+Vehicle bike2 = new Bike("Honda", "CBR", new DateTime(2010, 2, 10), 600);
 Vehicle truck = new Truck("Toyota", "Tundra", new DateTime(2010, 2, 10), 2000);
 
-int carRentalDays = 3;
-int bikeRentalDays = 8;
-int truckRentalDays = 2;
+List<Vehicle> vehicles = new List<Vehicle>();
+vehicles.Add(car);
+vehicles.Add(bike1);
+vehicles.Add(bike2);
+vehicles.Add(truck);
 
-double carRentalCost = car.Rent(carRentalDays);
-double bikeRentalCost = bike.Rent(bikeRentalDays);
-double truckRentalCost = truck.Rent(truckRentalDays);
+int RentalDays = 10;
 
-
-Console.WriteLine($"Car rental cost for {carRentalDays} days: ${carRentalCost}");
-Console.WriteLine($"Bike rental cost for {bikeRentalDays} days: ${bikeRentalCost}");
-Console.WriteLine($"Truck rental cost for {truckRentalDays} days: ${truckRentalCost}");
+foreach (Vehicle vehicle in vehicles)
+{
+  Console.WriteLine(vehicle.Rent(RentalDays));
+}
